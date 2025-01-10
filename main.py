e1b8e5e8ff60e7cbf74936c9a4e29235592c4230
hieube13 <hieubm.work@gmail.com>
2025-01-10 14:15:45 +0700

    fix 10-1-2025

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Cấu hình database
-DATABASE_URL = "sqlite:///./chat_history.db"
+DATABASE_URL = "sqlite:///./database/chat_history.db"
+# DATABASE_URL = "sqlite:///./chat_history.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
